6eccfe05efdc06c2c725486c8aea82ca79e19d43
Devin Howard <devin@callysto.com>
2017-05-06 18:08:56 -0700

    make `npm run test` work

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,5 +1,13 @@
 const config = require('./webpack.config')
 
 config.target = 'node'
+config.externals = config.externals.concat([
+  'react/lib/ExecutionEnvironment',
+  'react/lib/ReactContext',
+  'react/addons',
+  'react-test-renderer/shallow',
+  'react-dom/test-utils',
+  'canvas'
+])
 
 module.exports = config
